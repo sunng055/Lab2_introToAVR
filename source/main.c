1517ff6b9eb0293eabe6cad07b670537a2a42e54
Sunny Ng <sng055@wch136-24.cs.ucr.edu>
2021-04-11 14:47:00 -0700

    ini

---
+++ source/main.c
@@ -0,0 +1,49 @@
+/*	Author: sng055
+ *  Partner(s) Name: Sunny Ng
+ *	Lab Section:
+ *	Assignment: Lab #2  Exercise #2
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+DDRA = 0x00;
+DDRC = 0xFF;
+PORTA = 0xFF;
+PORTC = 0x00;
+unsigned char cntavail = 0;
+unsigned char tempA0 = 0x00;
+unsigned char tempA1 = 0x00;
+unsigned char tempA2 = 0x00;
+unsigned char tempA3 = 0x00;
+
+    /* Insert your solution below */
+    while (1) {
+	tempA0 = PINA & 0x01;
+	tempA1 = PINA & 0x02;
+	tempA2 = PINA & 0x04;
+	tempA3 = PINA & 0x08;
+	
+	if(tempA0 == 0x01) {
+	cntavail++;
+}
+	if(tempA1 == 0x02) {
+	cntavail++;
+}
+	if(tempA2 == 0x04) {
+	cntavail++;
+}
+	if(tempA3 == 0x08) {
+	cntavail++;
+}
+	PORTC = cntavail;
+    }
+    return 1;
+}
